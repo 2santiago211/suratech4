8073c750347a8d88c9ca8dd0ec7bae428070c5b9
Santiago Rodriguez Ocampo <2santiago0127@gmail.com>
2024-09-17 15:16:15 -0500

    se crea

---
+++ src/main/java/com/example/servidor_suratech_4/MODULOS/Paciente.java
@@ -0,0 +1,26 @@
+package com.example.servidor_suratech_4.MODULOS;
+
+import java.time.LocalDate;
+
+public class Paciente {
+  private   Integer id;
+
+  private   String nombres;
+
+  private Integer edad;
+
+  private   LocalDate fechaNacimiento;
+
+  private   String telefono;
+
+  private   Integer copago;
+
+  private   String email;
+
+  private   String direccion;
+
+  private   String ips;
+
+  private String medicoFamilia;
+
+}
